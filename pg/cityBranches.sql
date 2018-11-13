31c4f9297e0431a37404a392126a3e37b1c4f1cf
Juan Marcos Bellini <juanmbellini@gmail.com>
2018-11-12 22:54:27 -0300

    Add sorting by distance to each branch for a city

--- pg/cityBranches.sql
+++ pg/cityBranches.sql
@@ -1,11 +1,16 @@
 \timing
 SELECT
-    c.name                                              AS "City Name",
-    jsonb_pretty(json_agg(json_build_object(
-        'description', b.description,
-        'location', ST_AsText(b.location)
-    ))::jsonb)                                          AS "Branches near"
+    c.name                                              						AS "City Name",
+    jsonb_pretty(
+    	json_agg(
+    		json_build_object(
+        		'description', b.description,
+        		'location', ST_AsText(b.location),
+        		'distance', ST_Distance(b.location, c.location, :spheroid)
+    		) ORDER BY ST_Distance(b.location, c.location, :spheroid)
+    	)::jsonb
+    )                                          									AS "Branches near"
 FROM cities_geography as c, sube_branches_geography as b
 WHERE ST_DWithin(b.location, c.location, :distance, :spheroid)
-GROUP BY c.id, c.name
+GROUP BY c.id
 ORDER BY c.name;
