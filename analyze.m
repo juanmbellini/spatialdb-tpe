714527df8b05663afc6ee56ec0cf869b16de853f
Juan Marcos Bellini <juanmbellini@gmail.com>
2018-11-14 02:19:30 -0300

    Add an analysing script in octave
    - Performs the analysis of produced data (sum, mean, std, var, and median)

---
+++ analyze.m
@@ -0,0 +1,14 @@
+function [sums, means, stds, vars, medians, data] = analyze(data_dir, output_dir)
+	pg_geometry = dlmread(strcat(data_dir, '/', 'pg-geometry.txt'));
+	pg_geography_spheroid = dlmread(strcat(data_dir, '/', 'pg-geography-spheroid.txt'));
+	pg_geography_nospheroid = dlmread(strcat(data_dir, '/', 'pg-geography-nospheroid.txt'));
+	mongo_2D = dlmread(strcat(data_dir, '/', 'mongo-2D.txt'));
+	mongo_2DSphere = dlmread(strcat(data_dir, '/', 'mongo-2DSphere.txt'));
+
+	data = [pg_geometry, pg_geography_spheroid, pg_geography_nospheroid, mongo_2D, mongo_2DSphere];
+
+	sums = sum(data);
+	means = mean(data);
+	stds = std(data);
+	vars = var(data);
+	medians = var(data);
