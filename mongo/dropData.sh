6c9242312930e49ce3b37252c7b98db80da2f2ed
Juan Marcos Bellini <juanmbellini@gmail.com>
2018-11-14 00:11:56 -0300

    Change mongo data loader/dropper script
    - We add a collection that uses 2d geo index

--- mongo/dropData.sh
+++ mongo/dropData.sh
@@ -7,6 +7,6 @@ echo "Droping Data from MongoDB"
 mongo spatialdb-tpe \
     --port=27017 \
     --host=localhost \
-    --eval="db.cities.drop();db.sube_branches.drop();"
+    --eval="db.cities.drop();db.sube_branches_2d.drop();db.sube_branches_2dsphere.drop();"
 
 echo "Finished Data from MongoDB"
--- mongo/loadData.sh
+++ mongo/loadData.sh
@@ -10,14 +10,25 @@ CITIES_DATA_URL=${2:-$DEFAULT_CITIES_URL}
 
 echo "Loading Data to MongoDB"
 
-echo "Loading branches from" $BRANCHES_DATA_URL
+echo "Loading branches 2dsphere from" $BRANCHES_DATA_URL
 curl -s $BRANCHES_DATA_URL | mongoimport \
     --port=27017 \
     --host=localhost \
     --db=spatialdb-tpe \
-    --collection=sube_branches \
+    --collection=sube_branches_2dsphere \
     --jsonArray
-echo "Finished loading branches"
+echo "Finished loading branches 2dsphere"
+
+
+echo "Loading branches 2d from" $BRANCHES_DATA_URL
+curl -s $BRANCHES_DATA_URL | mongoimport \
+    --port=27017 \
+    --host=localhost \
+    --db=spatialdb-tpe \
+    --collection=sube_branches_2d \
+    --jsonArray
+echo "Finished loading branches 2d"
+
 
 echo "Loading cities from" $CITIES_DATA_URL
 curl -s $CITIES_DATA_URL | mongoimport \
@@ -28,12 +39,19 @@ curl -s $CITIES_DATA_URL | mongoimport \
     --jsonArray
 echo "Finished loading cities"
 
-echo "Creating geospatial index for branches collection"
+echo "Creating geospatial index for branches 2dsphere collection"
 mongo spatialdb-tpe \
     --port=27017 \
     --host=localhost \
-    --eval="db.sube_branches.createIndex({geometry: \"2dsphere\"});"
-echo "Finished creating geospatial index for branches collection"
+    --eval="db.sube_branches_2dsphere.createIndex({geometry: \"2dsphere\"});"
+echo "Finished creating geospatial index for branches 2dsphere collection"
+
+echo "Creating geospatial index for branches 2d collection"
+mongo spatialdb-tpe \
+    --port=27017 \
+    --host=localhost \
+    --eval="db.sube_branches_2d.createIndex({\"geometry.coordinates\": \"2d\"});"
+echo "Finished creating geospatial index for branches 2d collection"
 
 echo "Creating geospatial index for cities collection"
 mongo spatialdb-tpe \
